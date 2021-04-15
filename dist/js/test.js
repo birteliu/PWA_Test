ed47fe98a3fb50256ed61dc590622602b809f7d5
Birte Liu <zyi870213@gmail.com>
2021-04-15 11:15:58 +0800

    0415

--- dist/js/test.js
+++ dist/js/test.js
@@ -1,12 +1,5 @@
-// 判斷瀏覽器是否支援 Service Workers
-if('serviceWorker' in navigator) {
-    // 註冊 Service Workers
-    // register 是支援 Promise
-    navigator.serviceWorker.register('sw.js')
-      .then((messages)) => {
-          console.log('Service worker 註冊');
-      }).cathe((error) => {
-        console.log(error);
-      });
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../sw.js')
+    .then(reg => console.log('完成 SW 設定!', reg))
+    .catch(err => console.log('Error!', err));
   }
-  
--- src/js/sw.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener('fetch', (event) => {
-    /* empty */
-   });
-   
