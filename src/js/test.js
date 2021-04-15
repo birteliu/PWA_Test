c3129d2caee7dd898abe9391c21fb1c0570c4e1b
Birte Liu <zyi870213@gmail.com>
2021-04-15 14:01:09 +0800

    g

---
+++ src/js/test.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     // 判斷瀏覽器是否支援 Service Workers
+//     if ('serviceWorker' in navigator) {
+//       // register 是支援 Promise 的
+//       window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('./sw.js');
+//       });
+//     }
+//   });
