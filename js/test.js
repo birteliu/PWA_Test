ecce6d2b1759d6928e5acf0f296edd2f2d2ec5d7
Birte Liu <zyi870213@gmail.com>
2021-04-15 11:31:04 +0800

    0415

--- js/test.js
+++ js/test.js
@@ -1,5 +1,9 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../sw.js')
-    .then(reg => console.log('完成 SW 設定!', reg))
-    .catch(err => console.log('Error!', err));
-  }
+$(document).ready(function () {
+    // 判斷瀏覽器是否支援 Service Workers
+    if ('serviceWorker' in navigator) {
+      // register 是支援 Promise 的
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js');
+      });
+    }
+  });
--- sw.js
+++ sw.js
@@ -1,4 +1,26 @@
+
+// self.addEventListener('install', (event) => {
+// });
 self.addEventListener('fetch', (event) => {
-    /* empty */
-   });
-   
+
+});
+// self.addEventListener('refreshOffline', function (response) {
+//   return caches.open('offline2').then(function (cache) {
+//     return cache.put(offlinePage, response);
+//   });
+// });
+// self.addEventListener('push', function (event) {
+//   var data = event.data.json(); var opts = {
+//     body: data.body,
+//     icon: data.icon,
+//     data: {
+//       url: data.url
+//     }
+//   };
+//   event.waitUntil(self.registration.showNotification(data.title, opts));
+// });
+// self.addEventListener('notificationclick', function (event) {
+//   var data = event.notification.data; event.notification.close(); event.waitUntil(
+//     clients.openWindow(data.url)
+//   );
+// });
